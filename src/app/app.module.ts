236978e79046c8f98cbd9111c374ed93b82695a7
Ons Bouneb <onsbouneb1@gmail.com>
2019-01-20 15:28:01 +0100

    css2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,7 +44,7 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes,{useHash:true})
   ],
 
   exports: [
